cf9ea81bb3921f96e5adb1507b048a0fb117b8e7
taehee kim <taehee.kim@wonderwall.kr>
2023-10-15 16:19:19 +0900

    fix: 콘텐츠 손질

--- examples/binary-search/bs.lua
+++ examples/binary-search/bs.lua
@@ -1,20 +1,22 @@
---- @param array number[] 정렬된 number array table
---- @param target number 찾으려는 숫자
+---@param array number[] 정렬된 number array table
+---@param target number 찾으려는 숫자
 local function binary_search(array, target)
-    local function go(left, right)
-        if left > right then return -1 end
-        local mid = math.floor((right + left) / 2)
-        local value = array[mid]
+	local function go(left, right)
+		if left > right then
+			return -1
+		end
+		local mid = math.floor((right + left) / 2)
+		local value = array[mid]
 
-        if target == value then
-            return mid
-        elseif target < value then
-            return go(left, mid - 1)
-        else
-            return go(mid + 1, right)
-        end
-    end
-    return go(1, #array)
+		if target == value then
+			return mid
+		elseif target < value then
+			return go(left, mid - 1)
+		else
+			return go(mid + 1, right)
+		end
+	end
+	return go(1, #array)
 end
 
 return binary_search
